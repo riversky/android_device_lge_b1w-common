544e79d175125ed9a97502d781c8e083f13c2aea
xenius9 <xenius9@yandex.ru>
2016-03-28 14:38:39 +0300

    Initial commit for b1w based devices for cm-13.0

---
+++ rootdir/etc/set_baseband.sh
@@ -0,0 +1,12 @@
+#!/system/bin/sh
+
+product=`getprop ro.build.product`
+# grep the modem partition for baseband version and set it
+case "$product" in
+        "ls980" | "vs980")
+        setprop gsm.version.baseband `strings /firmware/image/modem.b18 | grep "^MPSS.DI" | head -1`
+        ;;
+        "d838" | "f350" | "f350k" | "f350l")
+        setprop gsm.version.baseband `strings /firmware/image/modem.b21 | grep "^M8974A-" | head -1`
+        ;;
+esac
