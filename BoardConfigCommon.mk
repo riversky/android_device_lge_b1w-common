aa73350f811d2103f342143075f8072cdd90f21b
xenius9 <xenius9@yandex.ru>
2016-05-24 12:50:22 +0300

    add dt2wake

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -66,7 +66,7 @@ COMMON_GLOBAL_CFLAGS += \
 
 # CMHW
 BOARD_HARDWARE_CLASS := device/lge/b1w-common/cmhw/
-TARGET_TAP_TO_WAKE_NODE := "/sys/devices/virtual/input/lge_touch/lpwg_notify"
+TARGET_TAP_TO_WAKE_NODE := "/sys/devices/virtual/input/lge_touch/dt2w_enable"
 
 # Display
 HAVE_ADRENO_SOURCE := false
