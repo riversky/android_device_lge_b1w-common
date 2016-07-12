8df6453cd6e8c5f660b05ca149b6083e718c21d0
xenius9 <xenius9@yandex.ru>
2016-07-12 07:54:37 +0300

    read wifi and bt mac from misc partition

--- hwaddrs/getmac.c
+++ hwaddrs/getmac.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011 The CyanogenMod Project
+ * Copyright (C) 2011-2015 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,13 +14,13 @@
  * limitations under the License.
  */
 
+#include <unistd.h>
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 
-/* Read plain address from NV and set the BT MAC
- * address accordingly */
+/* Read plain address from misc partiton and set the Wifi and BT mac addresses accordingly */
 
 int main() {
 	int fd1, fd2;
@@ -28,9 +28,24 @@ int main() {
 	char macbuf[3];
 	int i;
 
-	fd1 = open("/dev/block/platform/msm_sdcc.1/by-name/misc",O_RDONLY);
+	fd1 = open("/dev/block/bootdevice/by-name/misc",O_RDONLY);
+	fd2 = open("/data/misc/wifi/config",O_WRONLY|O_CREAT|O_TRUNC,S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH);
 
-	fd2 = open("/data/misc/bdaddr",O_WRONLY|O_CREAT|O_TRUNC,S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH);
+	write(fd2,"cur_etheraddr=",14);
+
+	for (i = 0; i<6; i++) {
+		lseek(fd1,0x3000+i,SEEK_SET);
+		lseek(fd2,0,SEEK_END);
+		read(fd1,&macbyte,1);
+		sprintf(macbuf,"%02x",macbyte);
+		write(fd2,&macbuf,2);
+		if (i!=5) write(fd2,":",1);
+	}
+
+	write(fd2,"\n",1);
+	close(fd2);
+
+	fd2 = open("/data/misc/bluetooth/bdaddr",O_WRONLY|O_CREAT|O_TRUNC,S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH);
 	for (i = 0; i<6; i++) {
 		lseek(fd1,0x4000+i,SEEK_SET);
 		lseek(fd2,0,SEEK_END);
